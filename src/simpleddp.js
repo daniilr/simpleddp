676c243533ef4a9d629dcf6b6d7570c494c7ce05
Daniil Ryzhkov <daniil.re@gmail.com>
2021-12-22 19:19:49 +0300

    Nanoid added to genereate ids

--- src/simpleddp.js
+++ src/simpleddp.js
@@ -7,13 +7,10 @@ import { fullCopy } from './helpers/fullCopy.js';
 import { ddpEventListener } from './classes/ddpEventListener.js';
 import { ddpSubscription } from './classes/ddpSubscription.js';
 import { ddpCollection } from './classes/ddpCollection.js';
+import { nanoid } from 'nanoid';
 
 function uniqueIdFuncGen() {
-	let idCounter = 0;
-
-	return function () {
-		return idCounter++;
-	}
+	return nanoid();
 }
 
 const simpleDDPcounter = uniqueIdFuncGen();
